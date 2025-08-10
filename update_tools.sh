4b190d2d755cc50f1ca092993d548cbbff4d7b34
Spy Schools <98182881+spyschools@users.noreply.github.com>
2025-08-11 01:55:30 +0700

    Update update_tools.sh

--- update_tools.sh
+++ update_tools.sh
@@ -2,53 +2,25 @@
 
 set -e
 
-echo "[*] Memulai update semua tools..."
-
-sudo apt update
-sudo apt upgrade -y
-
-# Update exploitdb
-if [ -d /opt/exploitdb ]; then
-  sudo git -C /opt/exploitdb pull
+echo "[*] Mulai update semua tools (root required)"
+if [[ $EUID -ne 0 ]]; then
+   echo "[-] Harus dijalankan sebagai root (sudo)" 
+   exit 1
 fi
 
-# Update exploitdb-papers
-if [ -d /opt/exploitdb-papers ]; then
-  sudo git -C /opt/exploitdb-papers pull
-fi
+# Update system packages
+apt update
+apt upgrade -y
 
-# Update beef-xss
-if [ -d /opt/beef ]; then
-  sudo git -C /opt/beef pull
-fi
+# Update all git repos under /opt
+for dir in /opt/*; do
+  if [ -d "$dir/.git" ]; then
+    echo "[*] Updating git repo $dir"
+    git -C "$dir" pull
+  fi
+done
 
-# Update paramspider
-if [ -d /opt/paramspider ]; then
-  sudo git -C /opt/paramspider pull
-fi
+# Update pip global packages
+pip3 list --outdated --format=freeze | cut -d = -f 1 | xargs -r pip3 install -U
 
-# Update eyewitness
-if [ -d /opt/eyewitness ]; then
-  sudo git -C /opt/eyewitness pull
-fi
-
-# Update getsploit
-if [ -d /opt/getsploit ]; then
-  sudo git -C /opt/getsploit pull
-fi
-
-# Update metagoofil
-if [ -d /opt/metagoofil ]; then
-  sudo git -C /opt/metagoofil pull
-fi
-
-# Update raccoon-scanner
-if [ -d /opt/raccoon-scanner ]; then
-  sudo git -C /opt/raccoon-scanner pull
-fi
-
-# Update pip packages (root)
-sudo pip3 install --upgrade h8mail spiderfoot waybackpy reconspider sn0int email2phonenumber
-
-echo "[*] Update selesai."
-
+echo "[*] Semua tools sudah diperbarui."
